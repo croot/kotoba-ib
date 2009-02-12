04004b54d61df349e97d17324aff87c57b2122e5
airflow83 <airflow83@c9e0983a-f3e1-11dd-8c4e-4bbb75a408b7>
2009-02-12 13:56:06 +0000

    Исправлен скрипт добавления доски. Протестирован вывод ошибок в скрипте создания треда и исправлен баг невозможности создания треда на пустой доске. Исправлен скрипт просмотра треда в соответстствии с новой системой сбора статистики. Другие мелкие исправления, комментарии.

--- kotoba-ib/add_board.php
+++ kotoba-ib/add_board.php
@@ -1,37 +1,53 @@
 <?php
+/*************************************
+ * Этот файл является частью Kotoba. *
+ * Файл license.txt содержит условия *
+ * распространения Kotoba.           *
+ *************************************/
+/*********************************
+ * This file is part of Kotoba.  *
+ * See license.txt for more info.*
+ *********************************/
+
+// Заметки:
+//
+// Наверное излишне будет добавлять сюда сбор статистики. Новые доски создаются очень редко.
+
+require 'common.php';
+
 $HEAD = 
 '<html>
 <head>
-	<title>Kotoba add board.</title>
+	<title>Kotoba добавление доски</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<link rel="stylesheet" type="text/css" href="kotoba.css">
+	<link rel="stylesheet" type="text/css" href="' . KOTOBA_DIR_PATH . '/kotoba.css">
 </head>
 <body>
 ';
+
 $FORM =	'
-<form action="add_board.php" method="post">
+<form action="' . KOTOBA_DIR_PATH . '/add_board.php" method="post">
 	<p>Boardname: 
 	<input name="Boardname" type="text" size="30" maxlength="16"> 
 	<input type="submit" value="Add">
 	</p>
 </form>
 ';
+
 $FOOTER = 
 '
 </body>
 </html>';
 
-ini_set('session.save_path', $_SERVER['DOCUMENT_ROOT'] . '/k/sessions/');
-ini_set('session.gc_maxlifetime', 60 * 60 * 24);
+ini_set('session.save_path', $_SERVER['DOCUMENT_ROOT'] . KOTOBA_DIR_PATH . '/sessions/');
+ini_set('session.gc_maxlifetime', 60 * 60 * 24);    // 1 день.
 ini_set('session.cookie_lifetime', 60 * 60 * 24);
 session_start();
 
-// For registred users only.
-if(isset($_SESSION['isLoggedIn']))
+if(isset($_SESSION['isLoggedIn']))  // Только для зарегистрированных пользователей.
 {
 	require 'database_connect.php';
 	
-	// Board addition.
 	if(isset($_POST['Boardname']))
 	{
 		$boardname_code   = RawUrlEncode($_POST['Boardname']);
@@ -39,54 +55,51 @@ if(isset($_SESSION['isLoggedIn']))
 
 		if($boardname_length >= 1 && $boardname_length <= 16 && strpos($boardname_code, '%') === false)
 		{
-			if(mysql_query('insert into `boards` (`Name`) values (\'' . $boardname_code . '\')') == false)
+			if(mysql_query("insert into `boards` (`Name`, `MaxPostNum`, `Board Settings`) values ('$boardname_code', 0, null)") == false)
 			{
-				$temp = '<p>Error.<br>Board addition falied by reason: ' . mysql_error() . '</p>';
+				$temp = '<span class="error">Ошибка. Добаление доски завершилось неудачей. Причина: ' . mysql_error() . '</span><br>';
 			}
 			else
 			{
-				mkdir($_SERVER['DOCUMENT_ROOT'] . '/k/' . $boardname_code . "/arch/", '0777', true);
-				mkdir($_SERVER['DOCUMENT_ROOT'] . '/k/' . $boardname_code . "/thumb/", '0777', true);
-				mkdir($_SERVER['DOCUMENT_ROOT'] . '/k/' . $boardname_code . "/img/", '0777', true);
-				$temp = '<p>' . $boardname_code . ' Successfully added.</p>';
+				mkdir($_SERVER['DOCUMENT_ROOT'] . KOTOBA_DIR_PATH . "/$boardname_code/arch/", '0777', true);
+				mkdir($_SERVER['DOCUMENT_ROOT'] . KOTOBA_DIR_PATH . "/$boardname_code/thumb/", '0777', true);
+				mkdir($_SERVER['DOCUMENT_ROOT'] . KOTOBA_DIR_PATH . "/$boardname_code/img/", '0777', true);
+				$temp = "<p>Доска $boardname_code успешно добавлена.</p>";
 			}
 		}
 		else
 		{
-			$temp = '<p>Wrong format.</p>';
+			$temp = '<span class="error">Ошибка. Неверный формат имени доски.</span><br>';
 		}
 	}
 
-	// Getting boards list.
-	if(($result = mysql_query('select `Name` from `boards`')) == false)
-	{
-		$BODY = '<p>Error.<br>Getting boards list falied by reason: ' . mysql_error() . '.</p>' . $FORM . $temp;
-	}
-	else
-	{
-		if(mysql_num_rows($result) == 0)
-		{
-			$BODY = '<p>There is no boards.</p>' . $FORM . $temp;
-		}
-		else
-		{
-			$BODY = "<p>Boards list: ";
-	
-			while ($row = mysql_fetch_array($result, MYSQL_NUM))
-			{
-	        	$BODY .= "[/<a href=\"$row[0]/\">$row[0]</a>/] ";
-			}
-	
-			$BODY = substr($BODY, 0, strlen($BODY) - 1);
+    if(($result = mysql_query('select `Name` from `boards` order by `Name`')) !== false)
+    {
+        if(mysql_num_rows($result) == 0)
+        {
+            $BODY = '<span class="error">Ошибка. Не создано ни одной доски.</span><br>' . $FORM . $temp;
+        }
+        else
+        {
+            $BODY = "<p>Список досок: ";
+            
+            while (($row = mysql_fetch_array($result, MYSQL_ASSOC)) !== false)
+                $BODY .= '/<a href="' . KOTOBA_DIR_PATH . "/$row[Name]/\">$row[Name]</a>/ ";
+
+            $BODY = substr($BODY, 0, strlen($BODY) - 1);
 			$BODY .= '</p>' . $FORM . $temp;
-	    }
+        }
 
-		mysql_free_result($result);
-	}
+        mysql_free_result($result);
+    }
+    else
+    {
+        $BODY = '<span class="error">Ошибка. Невозможно получить список досок. Причина: ' . mysql_error() . '.</span><br>' . $FORM . $temp;
+    }
 }
 else
 {
-	$BODY = '<p>Not logged in. Plz login here <a href="login.php">[login]</a></p>';
+	$BODY = '<span class="error">Залогиньтесь: <a href="login.php">[login]</a></span>';
 }
 
 $BODY .= '<br><a href="index.php">[Home]</a>';
--- kotoba-ib/threads.php
+++ kotoba-ib/threads.php
@@ -9,12 +9,12 @@
  * See license.txt for more info.*
  *********************************/
 
+require 'common.php';
+
 ini_set('session.save_path', $_SERVER['DOCUMENT_ROOT'] . KOTOBA_DIR_PATH  . '/sessions/');
 ini_set('session.gc_maxlifetime', 60 * 60 * 24);	// 1 день.
 ini_set('session.cookie_lifetime', 60 * 60 * 24);
 session_start();
-
-require 'common.php';
 
 $HEAD = 
 '<html>
@@ -35,22 +35,42 @@ if(KOTOBA_ENABLE_STAT)
     if(($stat_file = @fopen($_SERVER['DOCUMENT_ROOT'] . KOTOBA_DIR_PATH . '/threads.stat', 'a')) === false)
         die($HEAD . '<span class="error">Ошибка. Не удалось открыть или создать файл статистики.</span>' . $FOOTER);
 
-if(isset($_GET['b']) && isset($_GET['t']))
+require 'events.php';
+
+if(isset($_GET['b']))
 {
-	$BOARD_NAME = $_GET['b'];
-	$THREAD_NUM = $_GET['t'];
+    if(($BOARD_NAME = CheckFormat('board', $_GET['b'])) === false)
+	{
+		if(KOTOBA_ENABLE_STAT)
+			kotoba_stat(ERR_BOARD_BAD_FORMAT);
+
+		die($HEAD . '<span class="error">Ошибка. Имя доски имеет не верный формат.</span>' . $FOOTER);
+	}
 }
 else
 {
-	header("Location: $KOTOBA_DIR_PATH/");
-	exit;
+	if(KOTOBA_ENABLE_STAT)
+        kotoba_stat(ERR_BOARD_NOT_SPECIFED);
+
+	die($HEAD . '<span class="error">Ошибка. Не задано имя доски.</span>' . $FOOTER);
 }
 
-// Имя доски имеет не верный формат?
-if(CheckFormat('board', $BOARD_NAME) === false)
+if(isset($_GET['t']))
 {
-    header("Location: $KOTOBA_DIR_PATH/");
-    exit;
+    if(($THREAD_NUM = CheckFormat('thread', $_GET['t'])) === false)
+	{
+		if(KOTOBA_ENABLE_STAT)
+            kotoba_stat(ERR_THREAD_BAD_FORMAT);
+
+        die($HEAD . '<span class="error">Ошибка. Номер треда имеет не верный формат.</span>' . $FOOTER);
+	}
+}
+else
+{
+	if(KOTOBA_ENABLE_STAT)
+		kotoba_stat(ERR_THREAD_NOT_SPECIFED);
+
+	die($HEAD . '<span class="error">Ошибка. Не задан номер треда.</span>' . $FOOTER);
 }
 
 require 'database_connect.php';
@@ -66,18 +86,15 @@ $HEAD =
 <body>
 ";
 
-$FOOTER = 
-'
-</body>
-</html>';
-
 $REPLY_PASS = '';
 
 if(isset($_COOKIE['rempass']))
 {
 	if(($REPLY_PASS = CheckFormat('pass', $_COOKIE['rempass'])) === false)
 	{
-		kotoba_stat("(0037) Ошибка. Пароль для удаления имеет не верный формат.");
+        if(KOTOBA_ENABLE_STAT)
+            kotoba_stat(ERR_PASS_BAD_FORMAT);
+            
 		die($HEAD . '<span class="error">Ошибка. Пароль для удаления имеет не верный формат.</span>' . $FOOTER);
 	}
 }
@@ -101,153 +118,168 @@ $FORM =
 ";
 
 $BOARDS_LIST = '';
+$BOARD_NUM = -1;
 
 // Получение списка досок и проверка существут ли доска с заданным именем.
 if(($result = mysql_query('select `Name`, `id` from `boards` order by `Name`')) !== false)
 {
 	if(mysql_num_rows($result) == 0)
 	{
-		header("Location: $KOTOBA_DIR_PATH/");
-        exit;
+        if(KOTOBA_ENABLE_STAT)
+            kotoba_stat(ERR_BOARDS_NOT_EXIST);
+
+        die($HEAD . '<span class="error">Ошибка. Не создано ни одной доски.</span>' . $FOOTER);
 	}
 	else
 	{
-		$row = mysql_fetch_array($result, MYSQL_NUM);
-		
-		while ($row !== false)
+		while (($row = mysql_fetch_array($result, MYSQL_ASSOC)) !== false)
 		{
-			if($row[0] == $BOARD_NAME)
-			{
-				$exist = true;
-				$BOARD_NUM = $row[1];
-            }
+			if($row['Name'] == $BOARD_NAME)
+				$BOARD_NUM = $row['id'];
 
-            $BOARDS_LIST .= "/<a href=\"$KOTOBA_DIR_PATH/$row[0]/\">$row[0]</a>/ ";			
-			$row = mysql_fetch_array($result, MYSQL_NUM);
+            $BOARDS_LIST .= '/<a href="' . KOTOBA_DIR_PATH . "/$row[Name]/\">$row[Name]</a>/ ";
 		}
     }
 
 	mysql_free_result($result);
-	
-	if(!isset($exist))
+
+	if($BOARD_NUM == -1)
 	{
-		header("Location: $KOTOBA_DIR_PATH/");
-		exit;
-	}
+        if(KOTOBA_ENABLE_STAT)
+            kotoba_stat(sprintf(ERR_BOARD_NOT_FOUND, $BOARD_NAME));
+
+        die($HEAD . "<span class=\"error\">Ошибка. Доски с именем $BOARD_NAME не существует.</span>" . $FOOTER);
+    }
 }
 else
 {
-	$BOARDS_LIST = '<span class="error">Ошибка при получении списка досок. Причина: ' . mysql_error() . '.</span>';
+    if(KOTOBA_ENABLE_STAT)
+            kotoba_stat(sprintf(ERR_BOARDS_LIST, mysql_error()));
+
+	die($HEAD . '<span class="error">Ошибка. Невозможно получить список досок. Причина: ' . mysql_error() . '.</span>' . $FOOTER);
 }
 
 $MENU = $BOARDS_LIST . "<br>\n<h4 align=center>βchan</h4>\n<br><center><b>/$BOARD_NAME/$THREAD_NUM/</b></center>\n<hr>\n";
 
-// Имя треда имеет не верный формат.
-if(CheckFormat('thread', $THREAD_NUM) === false)
+// Проверка существования треда $THREAD_NUM на доске с именем $BOARD_NAME.
+if(($result = mysql_query("select `id` from `threads` where `id` = $THREAD_NUM and `board` = $BOARD_NUM")) !== false)
 {
-	header("Location: $KOTOBA_DIR_PATH/");
-	exit;
-}
-
-$THREAD = '';
-
-// Существует ли тред с номером $THREAD_NUM?
-if(($result = mysql_query('select `id` from `threads` where `id` = ' . $THREAD_NUM)) != false)
-{
-	// Треда с таким номером не существует.
-	if(mysql_num_rows($result) == 0)
+	if(mysql_num_rows($result) != 1)
 	{
-		header("Location: $KOTOBA_DIR_PATH/");
-		exit;
+        if(KOTOBA_ENABLE_STAT)
+			kotoba_stat(sprintf(ERR_THREAD_NOT_FOUND, $THREAD_NUM, $BOARD_NAME));
+			
+		mysql_free_result($result);
+		die($HEAD . "<span class=\"error\">Ошибка. Треда с номером $THREAD_NUM на доске $BOARD_NAME не найдено.</span>" . $FOOTER);
 	}
+    
+    mysql_free_result($result);
 }
 else
 {
-	$THREAD .= '<span class="error">Ошибка при проверке номера треда. Причина: ' . mysql_error() . '.</span><br>';
+	if(KOTOBA_ENABLE_STAT)
+        kotoba_stat(sprintf(ERR_THREAD_EXIST_CHECK, $THREAD_NUM, $BOARD_NAME, mysql_error()));
+    
+	die($HEAD . "<span class=\"error\">Ошибка. Не удалось проверить существание треда с номером $THREAD_NUM на доске $BOARD_NAME. Прична: " .  mysql_error() . "</error>" . $FOOTER);
 }
 
 // Получение постов просматриваемого треда.
-$query = "select `id`, `Time`, `Text`, `Post Settings` from `posts` where `thread` = $THREAD_NUM and `board` = " . $BOARD_NUM . " order by `id` asc";
+$query = "select `id`, `Time`, `Text`, `Post Settings` from `posts` where `thread` = $THREAD_NUM and `board` = $BOARD_NUM order by `id` asc";
+$THREAD = '';
 
-if(($posts = mysql_query($query)) != false)
+if(($posts = mysql_query($query)))
 {
 	if(mysql_num_rows($posts) > 0)
 	{
-		$isFirst = true;
-		$post = mysql_fetch_array($posts, MYSQL_NUM);		
-		$Op_settings = GetSettings('post', $post[3]);
+		//$isFirst = true;
+		$post = mysql_fetch_array($posts, MYSQL_ASSOC);
+		$Op_settings = GetSettings('post', $post['Post Settings']);
 
 		$THREAD .= "\n<div>\n";
-		$THREAD .= "<span class=\"filetitle\">" . $Op_settings['THEME'] . "</span> <span class=\"postername\">" . $Op_settings['NAME'] . "</span> " . $post[1];
+		$THREAD .= "<span class=\"filetitle\">$Op_settings[THEME]</span> <span class=\"postername\">$Op_settings[NAME]</span> $post[Time]";
 
 		if(isset($Op_settings['THEME']) && $Op_settings['THEME'] != '')
 			$HEAD = "<html>\n<head>\n
 				\t<title>Kotoba - $BOARD_NAME/$THREAD_NUM - " . $Op_settings['THEME'] . "</title>\n
 				\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n
 				\t<link rel=\"stylesheet\" type=\"text/css\" href=\"/k/kotoba.css\">\n
-				\t<script type=\"text/javascript\" src=\"$KOTOBA_DIR_PATH/kusaba.js\"></script>\n</head>\n<body>\n";
+				\t<script type=\"text/javascript\" src=\"" . KOTOBA_DIR_PATH . "/kusaba.js\"></script>\n</head>\n<body>\n";
 		
-		if(isset($Op_settings['IMGNAME']))
+		if(isset($Op_settings['IMGNAME']))  // С картинкой.
 		{
 			$img_thumb_filename = $Op_settings['IMGNAME'] . 't.' . $Op_settings['IMGEXT'];
 			$img_filename = $Op_settings['IMGNAME'] . '.' . $Op_settings['IMGEXT'];
 			
-			$THREAD .= " <span class=\"filesize\">Файл: <a target=\"_blank\" href=\"$KOTOBA_DIR_PATH/$BOARD_NAME/img/$img_filename\">$img_filename</a> -(<em>" .  $Op_settings['IMGSIZE'] . " Байт, " . $Op_settings['IMGSW'] . "x" . $Op_settings['IMGSH'] . "</em>)</span> <span class=\"reflink\"><span onclick=\"insert('>>$post[0]');\">#</span> <a href=\"$KOTOBA_DIR_PATH/$BOARD_NAME/$THREAD_NUM/#$post[0]\">$post[0]</a></span> <span class=\"delbtn\">[<a href=\"$KOTOBA_DIR_PATH/$BOARD_NAME/r$post[0]/\" title=\"Удалить\">×</a>]</span><a name=\"$post[0]\"></a>\n";
-			$THREAD .= "<br><a target=\"_blank\" href=\"$KOTOBA_DIR_PATH/$BOARD_NAME/img/$img_filename\"><img src=\"$KOTOBA_DIR_PATH/$BOARD_NAME/thumb/$img_thumb_filename\" class=\"thumb\" width=\"" . $Op_settings['IMGTW'] . "\" heigth=\"" . $Op_settings['IMGTH'] . "\"></a>";
-			$THREAD .= "<blockquote>\n" . ($post[2] == "" ? "<br>" : $post[2]) . "\n</blockquote>\n";
+			$THREAD .= " <span class=\"filesize\">Файл: <a target=\"_blank\" href=\"" . KOTOBA_DIR_PATH . "/$BOARD_NAME/img/$img_filename\">$img_filename</a> -(<em>$Op_settings[IMGSIZE] Байт, $Op_settings[IMGSW]x$Op_settings[IMGSH]</em>)</span> <span class=\"reflink\"><span onclick=\"insert('>>$post[id]');\">#</span> <a href=\"" . KOTOBA_DIR_PATH . "/$BOARD_NAME/$THREAD_NUM/#$post[id]\">$post[id]</a></span> <span class=\"delbtn\">[<a href=\" " . KOTOBA_DIR_PATH . "/$BOARD_NAME/r$post[id]/\" title=\"Удалить\">×</a>]</span><a name=\"$post[id]\"></a>\n";
+			$THREAD .= "<br><a target=\"_blank\" href=\"" . KOTOBA_DIR_PATH . "/$BOARD_NAME/img/$img_filename\"><img src=\"" . KOTOBA_DIR_PATH . "/$BOARD_NAME/thumb/$img_thumb_filename\" class=\"thumb\" width=\"$Op_settings[IMGTW]\" heigth=\"$Op_settings[IMGTH]\"></a>";
+			$THREAD .= "<blockquote>\n" . ($post['Text'] == "" ? "<br>" : $post['Text']) . "\n</blockquote>\n";
 		}
 		else
 		{
-			$THREAD .= " <span class=\"reflink\"><span onclick=\"insert('>>$post[0]');\">#</span> <a href=\"$KOTOBA_DIR_PATH/$BOARD_NAME/$THREAD_NUM/#$post[0]\">$post[0]</a></span> <span class=\"delbtn\">[<a href=\"$KOTOBA_DIR_PATH/$BOARD_NAME/r$post[0]/\" title=\"Удалить\">×</a>]</span><a name=\"$post[0]\"></a>\n";
-			$THREAD .= "<br><blockquote>\n" . ($post[2] == "" ? "<br>" : $post[2]) . "\n</blockquote>\n";
+			$THREAD .= " <span class=\"reflink\"><span onclick=\"insert('>>$post[id]');\">#</span> <a href=\"" . KOTOBA_DIR_PATH . "/$BOARD_NAME/$THREAD_NUM/#$post[id]\">$post[id]</a></span> <span class=\"delbtn\">[<a href=\"" . KOTOBA_DIR_PATH . "/$BOARD_NAME/r$post[id]/\" title=\"Удалить\">×</a>]</span><a name=\"$post[id]\"></a>\n";
+			$THREAD .= "<br><blockquote>\n" . ($post['Text'] == "" ? "<br>" : $post['Text']) . "\n</blockquote>\n";
         }
 
 		$THREAD .= "<div>\n";
 		
-		$post = mysql_fetch_array($posts, MYSQL_NUM);
-
-		while ($post)
+		while (($post = mysql_fetch_array($posts, MYSQL_ASSOC)) !== false)
 		{
-			$Replay_settings = GetSettings('post', $post[3]);
+			$Replay_settings = GetSettings('post', $post['Post Settings']);
 
 			$THREAD .= "<table>\n";
-			$THREAD .= "<tr>\n\t<td class=\"reply\"><span class=\"filetitle\">" . $Replay_settings['THEME'] . "</span> <span class=\"postername\">" . $Replay_settings['NAME'] . "</span>  " . $post[1];
+			$THREAD .= "<tr>\n\t<td class=\"reply\"><span class=\"filetitle\">$Replay_settings[THEME]</span> <span class=\"postername\">$Replay_settings[NAME]</span> $post[Time]";
 			
 			if(isset($Replay_settings['IMGNAME']))
 			{
 				$img_thumb_filename = $Replay_settings['IMGNAME'] . 't.' . $Replay_settings['IMGEXT'];
 				$img_filename = $Replay_settings['IMGNAME'] . '.' . $Replay_settings['IMGEXT'];
 
-				$THREAD .= " <span class=\"filesize\">Файл: <a target=\"_blank\" href=\"$KOTOBA_DIR_PATH/$BOARD_NAME/img/$img_filename\">$img_filename</a> -(<em>" .  $Replay_settings['IMGSIZE'] . " Байт " . $Replay_settings['IMGSW'] . "x" . $Replay_settings['IMGSH'] . "</em>)</span> <span class=\"reflink\"><span onclick=\"insert('>>$post[0]');\">#</span> <a href=\"$KOTOBA_DIR_PATH/$BOARD_NAME/$THREAD_NUM/#$post[0]\">$post[0]</a></span> <span class=\"delbtn\">[<a href=\"$KOTOBA_DIR_PATH/$BOARD_NAME/r$post[0]/\" title=\"Удалить\">×</a>]</span><a name=\"$post[0]\"></a>\n";
+				$THREAD .= " <span class=\"filesize\">Файл: <a target=\"_blank\" href=\"". KOTOBA_DIR_PATH . "/$BOARD_NAME/img/$img_filename\">$img_filename</a> -(<em>$Replay_settings[IMGSIZE] Байт $Replay_settings[IMGSW]x$Replay_settings[IMGSH]</em>)</span> <span class=\"reflink\"><span onclick=\"insert('>>$post[id]');\">#</span> <a href=\"" . KOTOBA_DIR_PATH . "/$BOARD_NAME/$THREAD_NUM/#$post[id]\">$post[id]</a></span> <span class=\"delbtn\">[<a href=\"" . KOTOBA_DIR_PATH. "/$BOARD_NAME/r$post[id]/\" title=\"Удалить\">×</a>]</span><a name=\"$post[id]\"></a>\n";
 				$THREAD .= "\t<br><a target=\"_blank\" href=\"$KOTOBA_DIR_PATH/$BOARD_NAME/img/$img_filename\"><img src=\"$KOTOBA_DIR_PATH/$BOARD_NAME/thumb/$img_thumb_filename\" class=\"thumb\" width=\"" . $Replay_settings['IMGTW'] . "\" heigth=\"" . $Replay_settings['IMGTH'] . "\"></a>";
-				$THREAD .= "<blockquote>\n" . ($post[2] == "" ? "<br>" : $post[2]) . "</blockquote>\n\t</td>\n</tr>\n";
+				$THREAD .= "<blockquote>\n" . ($post['Text'] == "" ? "<br>" : $post['Text']) . "</blockquote>\n\t</td>\n</tr>\n";
 			}
 			else
 			{
-				$THREAD .= " <span class=\"reflink\"><span onclick=\"insert('>>$post[0]');\">#</span> <a href=\"$KOTOBA_DIR_PATH/$BOARD_NAME/$THREAD_NUM/#$post[0]\">$post[0]</a></span> <span class=\"delbtn\">[<a href=\"$KOTOBA_DIR_PATH/$BOARD_NAME/r$post[0]/\" title=\"Удалить\">×</a>]</span><a name=\"$post[0]\"></a>\n";
-				$THREAD .= "\t<br><blockquote>\n" . ($post[2] == "" ? "<br>" : $post[2]) . "</blockquote>\n\t</td>\n</tr>\n";
+				$THREAD .= " <span class=\"reflink\"><span onclick=\"insert('>>$post[id]');\">#</span> <a href=\"" . KOTOBA_DIR_PATH . "/$BOARD_NAME/$THREAD_NUM/#$post[id]\">$post[id]</a></span> <span class=\"delbtn\">[<a href=\"" . KOTOBA_DIR_PATH . "/$BOARD_NAME/r$post[id]/\" title=\"Удалить\">×</a>]</span><a name=\"$post[id]\"></a>\n";
+				$THREAD .= "\t<br><blockquote>\n" . ($post['Text'] == "" ? "<br>" : $post['Text']) . "</blockquote>\n\t</td>\n</tr>\n";
             }
 			
 			$THREAD .= "</table>\n";
-
-			$post = mysql_fetch_array($posts, MYSQL_NUM);
-		}
+        }
 
 		$THREAD .= "</div>\n</div>\n<br clear=\"left\">\n<hr>";
 	}
+    else
+    {
+        if(KOTOBA_ENABLE_STAT)
+            kotoba_stat(sprintf(ERR_THREAD_BOARD_POSTS, $THREAD_NUM, $BOARD_NAME));
+
+        die($HEAD . "<span class=\"error\">Ошибка. В треде $THREAD_NUM на доске $BOARD_NAME нет ни одного поста.</span>" . $FOOTER);
+    }
 
 	mysql_free_result($posts);
 }
 else
 {
-	$THREAD .= '<span class="error">Ошибка при получении постов. Причина: ' . mysql_error() . '.</span>';
+    if(KOTOBA_ENABLE_STAT)
+        kotoba_stat(sprintf(ERR_GET_THREAD_POSTS, $THREAD_NUM, $BOARD_NAME, mysql_error()));
+
+    die($HEAD . "<span class=\"error\">Ошибка. Невозможно получить посты для предпросмотра треда $thread[id] доски $BOARD_NAME. Причина: " . mysql_error() . '.</span>' . $FOOTER);
 }
 
 echo $HEAD . $MENU . $FORM . '<hr>' . $THREAD . $FOOTER;
+?>
+<?php
 
-function exit_with($err_msg)
+/*
+ * Выводит сообщение $errmsg в файл статистики $stat_file.
+ *
+ */
+function kotoba_stat($errmsg)
 {
-	global $HEAD, $MENU, $FORM, $FOOTER;
-	die ($HEAD . $MENU . $FORM . '<hr>' . $err_msg . '<hr>' . $FOOTER);
+    global $stat_file;
+    fwrite($stat_file, "$errmsg (" . date("Y-m-d H:i:s") . ")\n");
+    //fclose($stat_file);
 }
+
 ?>
