a4c12a15048a2c61cfe503628b6cf1df2d04832a
eeyore@list.ru <eeyore@list.ru@c9e0983a-f3e1-11dd-8c4e-4bbb75a408b7>
2009-04-29 12:04:58 +0000

    login, logout and register rewritten with templates

--- trunk/kotoba-ib/login.php
+++ trunk/kotoba-ib/login.php
@@ -11,48 +11,26 @@
 
 require 'config.php';
 require 'common.php';
+require 'error_processing.php';
+require 'events.php';
 
-$HEAD = '<html>
-<head>
-	<title>Kotoba Login</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<link rel="stylesheet" type="text/css" href="' . KOTOBA_DIR_PATH . '/kotoba.css">
-</head>
-<body>
-';
-
-$BODY = '
-<form action="' . KOTOBA_DIR_PATH . '/login.php" method="post">
-<p align="center">
-<table width="80%" align="center">
-	<div class="pass">
-		<center>
-			<img src="' . KOTOBA_DIR_PATH . '/img/logo.png"><br>
-			Keyword: <input name="Keyword" type="text" maxlength="32">
-			<input type="submit" value="Login">
-			<br><br><br><br><br><br><br><br><br><br>
-			<small>- Kotoba 0.00.α -</small>
-		</center>
-	</div>
-</table>
-</form>
-';
-
-$FOOTER = '
-</body>
-</html>';
 
 if(KOTOBA_ENABLE_STAT)
     if(($stat_file = @fopen($_SERVER['DOCUMENT_ROOT'] . KOTOBA_DIR_PATH . '/login.stat', 'a')) == false)
-        die($HEAD . '<span class="error">Ошибка. Неудалось открыть или создать файл статистики.</span>' . $FOOTER);
+        kotoba_error("Ошибка. Неудалось открыть или создать файл статистики.");
 
 ini_set('session.save_path', $_SERVER['DOCUMENT_ROOT'] . KOTOBA_DIR_PATH . '/sessions/');
 ini_set('session.gc_maxlifetime', 60 * 60 * 24);
 ini_set('session.cookie_lifetime', 60 * 60 * 24);
 session_start();
 
-if(isset($_SESSION['isLoggedIn']))
-	die($HEAD . "<p>\n\tВы уже вошли.<br>\n\t<a href=\"" . KOTOBA_DIR_PATH . "/logout.php\">Выйти</a>\n</p>" . $FOOTER);
+$smarty = new SmartyKotobaSetup();
+if(isset($_SESSION['isLoggedIn'])) {
+	$smarty->assign('form', 0);
+	$smarty->assign('message', 'Вы уже вошли');
+	$smarty->display('login.tpl');
+	exit;
+}
 
 if(isset($_POST['Keyword']))
 {
@@ -63,25 +41,33 @@ if(isset($_POST['Keyword']))
 	{
 		$keyword_hash = md5($keyword_code);
 		require 'databaseconnect.php';
-
-		if(($result = mysql_query("select `id` from `users` where `Key` = '$keyword_hash'")) != false)
+		$sql = sprintf("select id from users where `Key` = '%s'", $keyword_hash);
+		if(($result = mysql_query($sql)) != false)
 		{
-			if(@mysql_num_rows($result) == 0)
-				$BODY .= "<p>\n\tОшибка. Вы не зарегистрированы.<br>\n\t<a href=\"" . KOTOBA_DIR_PATH . "/index.php\">На главную</a>\n</p>";
+			if(@mysql_num_rows($result) == 0) {
+				$smarty->assign('message', 'Ошибка. Вы не зарегистрированы.');
+				$smarty->assign('form', 0);
+				$smarty->display('login.tpl');
+				exit;
+			}
 			else
 			{
-				if(mysql_query('update `users` set `SID` = "' . session_id() . "\" where `key` = '$keyword_hash'") == false)
+				$sql = sprintf("update users set SID = '%s' where `Key` = '%s'",
+					session_id(), $keyword_hash);
+				if(mysql_query($sql) == false)
 				{
 					if(KOTOBA_ENABLE_STAT)
 						kotoba_stat(sprintf(ERR_UPDATE_USER_SID, mysql_error()));
 
-					die($HEAD . '<span class="error">Ошибка. Неудалось обновить идентификатор сессии пользователя. Причина: ' . mysql_error() . '.</span>' . $FOOTER);
+					kotoba_error(sprintf(ERR_UPDATE_USER_SID, mysql_error()));
 				}
 				else
 				{
 					$_SESSION['isLoggedIn'] = session_id();
-					$BODY = "<p>Вы вошли.<br>\n\t<a href=\"" . KOTOBA_DIR_PATH . "/logout.php\">Выйти</a><br>\n\t" .
-							"<a href=\"" . KOTOBA_DIR_PATH . "/index.php\">На главную</a>\n</p>";
+					$smarty->assign('message', 'Вы успешно вошли.');
+					$smarty->assign('form', 0);
+					$smarty->display('login.tpl');
+					exit;
 				}
 			}
 		}
@@ -90,16 +76,20 @@ if(isset($_POST['Keyword']))
 			if(KOTOBA_ENABLE_STAT)
 				kotoba_stat(sprintf(ERR_USER_DATA, mysql_error()));
 
-			die($HEAD . '<span class="error">Ошибка. Невозможно получить данные пользователя. Причина: ' . mysql_error() . '.</span>' . $FOOTER);
+			kotoba_error(sprintf(ERR_USER_DATA, mysql_error()));
 		}
 	}
-	else
-		$BODY .= "<p>\n\tОшибка. Ключевое слово должно иметь длину 16-32 свимолов A-Z a-z 0-9 _ -\n</p>";
+	else {
+		$smarty->assign('message', 'Ошибка. Ключевое слово должно иметь длину 16-32 свимолов A-Z a-z 0-9 _ -');
+		$smarty->assign('form', 0);
+		$smarty->display('login.tpl');
+		exit;
+	}
 }
 
-echo $HEAD . $BODY . $FOOTER;
-?>
-<?php
+//$smarty->assign('message', 'Ошибка. Вы не зарегистрированы.');
+$smarty->assign('form', 1);
+$smarty->display('login.tpl');
 /*
  * Выводит сообщение $errmsg в файл статистики $stat_file.
  */
