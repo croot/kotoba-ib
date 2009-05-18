2d8fcf58c91fc5488167d3c8e29851f64ec103ef
eeyore@list.ru <eeyore@list.ru@c9e0983a-f3e1-11dd-8c4e-4bbb75a408b7>
2009-05-18 14:51:40 +0000

    new database schema concept, not ready yet

--- trunk/kotoba-ib/res/concept/database/kotoba-tables.sql
+++ trunk/kotoba-ib/res/concept/database/kotoba-tables.sql
@@ -1,4 +1,7 @@
+/* DO NOT USE ME*/
 delimiter ;
+select "do not use me!";
+
 use kotoba2
 
 drop table if exists boards;
@@ -6,12 +9,86 @@ CREATE TABLE boards (
 	id int AUTO_INCREMENT NOT NULL,
 	board_name varchar(16) NOT NULL,
 	board_description varchar(50) NULL,
-	bump_limit int NOT NULL DEFAULT 10,
+	board_title varchar(50) NOT NULL,
 	threads int NULL DEFAULT 0,
-	max_threads int NOT NULL DEFAULT 10,
-	PRIMARY KEY (id)
+	bump_limit int NOT NULL DEFAULT 10,
+	rubber_board tinyint NOT NULL DEFAULT 0,
+	visible_threads int NOT NULL DEFAULT 10,
+	same_upload varchar(32) not null default 'yes',
+	PRIMARY KEY (id),
+	unique index IX_boards (board_name)
 ) engine=innodb;
-CREATE UNIQUE INDEX IX_boards ON boards (board_name);
+-- CREATE UNIQUE INDEX IX_boards ON boards (board_name);
+/*
+boards: boards in your kotoba
+id - board identifier
+board_name - board name ie 'a', 'b'
+board_description - board description
+board_title - board title in board preview
+bump_limit - default bump limit in thread
+threads - not deleted, not archived threads (for statistics)
+rubber_board - boolean value, if 1, threads doesn't archived automatically
+visible_threads - how many threads show in board preview. should be ignored if rubber_board is set
+same_upload - allow upload same binary files. values: 'yes' - allow, 'no' - disallow and 'once' - use once uploaded file for all instances
+*/
+
+drop table if exists upload_types;
+create table upload_types (
+	id int auto_increment not null,
+	extension varchar(10) not null,
+	store_extension varchar(10) null,
+	handler varchar(64) not null default 'internal',
+	thumbnail_image varchar(256) not null default 'unknown.png',
+	primary key(id)
+) engine=innodb;
+
+/*
+upload_types: supported binary files for upload and how to handle them
+id - upload type identifier
+extension - binary file extension
+store_extension - binary file should be stored with this extension ('jpeg' as 'jpg')
+handler - how to handle files. values:
+	'store' - store file, thumbnail use from thumbnail_image field,
+	'internal' - use kotoba internal thumbnail routine (make sure file format supported!),
+	'internal_png' use kotoba internal thumbnail routine which makes png (also check availability!).
+thumbnail_image - (default) thumbnail image for this binary file
+*/
+
+drop table if exists board_upload_types;
+create table board_upload_types(
+	board_id int not null,
+	upload_id int not null,
+	unique index IX_but (board_id, upload_id),
+	foreign key (board_id) references boards(id) on delete cascade,
+	foreign key (upload_id) references upload_types(id) on delete cascade
+) engine=innodb;
+/*
+board_upload_types: reduction table. stores information about board binary upload supported.
+board_id - board id
+upload_id - upload type id
+board | upload_type
++---+---+
+| 1 | 1 |
++---+---+
+| 1 | 2 |
++---+---+
+etc...
+*/
+
+drop table if exists uploads;
+CREATE TABLE uploads (
+	id int auto_increment not null,
+	board_id int not null,
+	post_id int not null,
+	hash varchar(32) not null,
+	file_name varchar(256) not null,
+	file_x int,
+	file_y, int,
+	thumbnail varchar(256) not null,
+	thumbnail_x int,
+	thumbnail_y int
+) engine=innodb;
+
 
 drop table if exists threads;
 CREATE TABLE threads (
