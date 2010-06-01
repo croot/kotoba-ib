57ba3755b461a805b9316f60ffb58168d3c744ea
sorcchan <sorcchan@c9e0983a-f3e1-11dd-8c4e-4bbb75a408b7>
2010-06-01 05:47:52 +0000

    Macrochan integration begins.

---
+++ kotoba-ib/res/macrochan_data.php
@@ -0,0 +1,23 @@
+<?php
+// Тестовые данные с макрочана.
+
+$MACROCHAN_TAGS = array(
+    array(1, 'orgasm_face'),
+    array(2, 'persona_4'),
+    array(3, 'rozen_maiden'),
+    array(4, 'суисейсеки')
+);
+
+$MACROCHAN_IMAGES = array(
+    array(1, '3478.jpg', '640', '480', 63290, '3478.jpg', '192', '144'),
+    array(2, '3377.png', '1200', '1350', 460347, '3377.jpg', '171', '192')
+);
+
+$MACROCHAN_TAGS_IMAGES = array(
+    array(1, 1),
+    array(3, 1),
+    array(4, 1),
+    array(1, 2),
+    array(2, 2)
+);
+?>
