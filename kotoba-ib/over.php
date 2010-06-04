abf1f0fa8dc2c5193e1711e371f724255d067ec9
sorcchan <sorcchan@c9e0983a-f3e1-11dd-8c4e-4bbb75a408b7>
2010-06-04 09:52:49 +0000

    Overchan data script.

---
+++ kotoba-ib/over.php
@@ -0,0 +1,41 @@
+<?php
+/*************************************
+ * Этот файл является частью Kotoba. *
+ * Файл license.txt содержит условия *
+ * распространения Kotoba.           *
+ *************************************/
+/*********************************
+ * This file is part of Kotoba.  *
+ * See license.txt for more info.*
+ *********************************/
+// Скрипт, предоставляющий данные для оверчана.
+require_once 'config.php';
+require_once Config::ABS_PATH . '/lib/errors.php';
+require Config::ABS_PATH . '/locale/' . Config::LANGUAGE . '/errors.php';
+require_once Config::ABS_PATH . '/lib/db.php';
+try {
+    $boards = boards_get_all();
+    $categories = categories_get_all();
+
+    $out = '[';
+    foreach ($categories as $category) {
+        foreach ($boards as $board) {
+            if ($category['id'] == $board['category']) {
+                $out .= "<a href=\"/{$board['name']}/\">{$board['name']}</a> /\n";
+            }
+        }
+        $out = mb_substr($out, 0, mb_strlen($out, Config::MB_ENCODING) - 3, Config::MB_ENCODING);
+        $out .= " |\n";
+    }
+    $out = mb_substr($out, 0, mb_strlen($out, Config::MB_ENCODING) - 3, Config::MB_ENCODING);
+    $out .= ']';
+    echo $out;
+
+    DataExchange::releaseResources();
+    exit(0);
+} catch(Exception $e) {
+    $smarty->assign('msg', $e->__toString());
+    DataExchange::releaseResources();
+    die($smarty->fetch('error.tpl'));
+}
+?>
