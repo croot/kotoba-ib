5f19f30016d0d585adfe09961e8fe75a29a5ddc1
sorcchan <sorcchan@c9e0983a-f3e1-11dd-8c4e-4bbb75a408b7>
2010-06-09 05:37:59 +0000

    Исправление основных скриптов.

--- trunk/kotoba-ib/admin/edit_boards.php
+++ trunk/kotoba-ib/admin/edit_boards.php
@@ -57,63 +57,65 @@ try {
             $new_annotation = boards_check_annotation($_POST['new_annotation']);
             $new_bump_limit = boards_check_bump_limit($_POST['new_bump_limit']);
             if (isset($_POST['new_force_anonymous'])) {
-                $new_force_anonymous = '1';
+                $new_force_anonymous = 1;
             } else {
-                $new_force_anonymous = '0';
+                $new_force_anonymous = 0;
             }
             $new_default_name = boards_check_default_name($_POST['new_default_name']);
             if (isset($_POST['new_with_files'])) {
-                $new_with_files = '1';
+                $new_with_files = 1;
             } else {
-                $new_with_files = '0';
+                $new_with_files = 0;
             }
             if (isset($_POST['new_macro'])) {
-                $new_macro = '1';
+                $new_macro = 1;
             } else {
-                $new_macro = '0';
+                $new_macro = 0;
             }
             if (isset($_POST['new_youtube'])) {
-                $new_youtube = '1';
+                $new_youtube = 1;
             } else {
-                $new_youtube = '0';
+                $new_youtube = 0;
             }
             if (isset($_POST['new_captcha'])) {
-                $new_captcha = '1';
+                $new_captcha = 1;
             } else {
-                $new_captcha = '0';
+                $new_captcha = 0;
             }
-			$new_same_upload = boards_check_same_upload($_POST['new_same_upload']);
-			$new_popdown_handler = popdown_handlers_check_id($_POST['new_popdown_handler']);
-			$new_category = categories_check_id($_POST['new_category']);
+            $new_same_upload = boards_check_same_upload($_POST['new_same_upload']);
+            $new_popdown_handler = popdown_handlers_check_id($_POST['new_popdown_handler']);
+            $new_category = categories_check_id($_POST['new_category']);
+
+            /*
+             * Проверим, нет ли уже доски с таким именем. Если есть, то изменим
+             * параметры существующей.
+             */
+            $found = false;
+            foreach ($boards as $board) {
+                if ($board['name'] == $new_name && $found = true) {
+                    boards_edit($board['id'], $new_title, $new_annotation,
+                            $new_bump_limit, $new_force_anonymous,
+                            $new_default_name, $new_with_files, $new_macro,
+                            $new_youtube, $new_captcha, $new_same_upload,
+                            $new_popdown_handler, $new_category);
+                    $reload_boards = true;
+                    break;
+                }
+            }
+            if (!$found) {
+                boards_add($new_name, $new_title, $new_annotation,
+                        $new_bump_limit, $new_force_anonymous,
+                        $new_default_name, $new_with_files, $new_macro,
+                        $new_youtube, $new_captcha, $new_same_upload,
+                        $new_popdown_handler, $new_category);
+                create_directories($new_name);
+                $reload_boards = true;
+            }
+            }// Создание новой доски.
+
+        // Изменение параметров существующих досок.
+		foreach ($boards as $board) {
             
-			/*
-			 * Проверим, нет ли уже доски с таким именем. Если есть, то изменим
-			 * параметры существующей.
-			 */
-			$found = false;
-			foreach($boards as $board)
-				if($board['name'] == $new_name && $found = true)
-				{
-					boards_edit($board['id'], $new_title, $new_annotation, $new_bump_limit,
-						$new_force_anonymous, $new_default_name,
-						$new_with_files, $new_macro, $new_youtube, $new_captcha, $new_same_upload, $new_popdown_handler,
-						$new_category);
-					$reload_boards = true;
-					break;
-				}
-			if(!$found)
-			{
-				boards_add($new_name, $new_title, $new_annotation, $new_bump_limit,
-					$new_force_anonymous, $new_default_name,
-					$new_with_files, $new_macro, $new_youtube, $new_captcha, $new_same_upload, $new_popdown_handler,
-					$new_category);
-				create_directories($new_name);
-				$reload_boards = true;
-			}
-		}// Создание новой доски.
-// Изменение параметров существующих досок.
-		foreach($boards as $board)
-		{
 			// Был ли изменён заголовок доски?
 			$param_name = "title_{$board['id']}";
 			$new_title = $board['title'];
@@ -269,27 +271,28 @@ try {
 				$reload_boards = true;
 			}
 		}// Изменение параметров существующих досок.
-// Удаление выбранных досок.
-		foreach($boards as $board)
-			if(isset($_POST["delete_{$board['id']}"]))
-			{
-				boards_delete($board['id']);
-				$reload_boards = true;
-			}
+
+        // Удаление выбранных досок.
+        foreach ($boards as $board) {
+            if (isset($_POST["delete_{$board['id']}"])) {
+                boards_delete($board['id']);
+                $reload_boards = true;
+            }
+        }
 	}
-// Вывод формы редактирования.
-	if($reload_boards)
-		$boards = boards_get_all();
-	DataExchange::releaseResources();
-	$smarty->assign('popdown_handlers', $popdown_handlers);
-	$smarty->assign('categories', $categories);
-	$smarty->assign('boards', $boards);
-	$smarty->display('edit_boards.tpl');
-}
-catch(Exception $e)
-{
-	$smarty->assign('msg', $e->__toString());
-	DataExchange::releaseResources();
-	die($smarty->fetch('error.tpl'));
+
+    // Вывод формы редактирования.
+    if ($reload_boards) {
+        $boards = boards_get_all();
+    }
+    DataExchange::releaseResources();
+    $smarty->assign('popdown_handlers', $popdown_handlers);
+    $smarty->assign('categories', $categories);
+    $smarty->assign('boards', $boards);
+    $smarty->display('edit_boards.tpl');
+} catch(Exception $e) {
+    $smarty->assign('msg', $e->__toString());
+    DataExchange::releaseResources();
+    die($smarty->fetch('error.tpl'));
 }
 ?>
