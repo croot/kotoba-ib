492384ab7c6ad0dcb1e764180973b3d2d0dc38e7
sorcchan <sorcchan@c9e0983a-f3e1-11dd-8c4e-4bbb75a408b7>
2010-08-17 15:20:33 +0000

    Hide unhide threads. Bug with default locale name in config.default

--- kotoba-ib/unhide_thread.php
+++ kotoba-ib/unhide_thread.php
@@ -8,7 +8,12 @@
  * This file is part of Kotoba.  *
  * See license.txt for more info.*
  *********************************/
-// Скрипт отмены скрытия нити.
+
+/**
+ * Скрипт отмены скрытия нити.
+ * @package userscripts
+ */
+
 require 'config.php';
 require_once Config::ABS_PATH . '/lib/errors.php';
 require Config::ABS_PATH . '/locale/' . Config::LANGUAGE . '/errors.php';
@@ -17,43 +22,49 @@ require Config::ABS_PATH . '/locale/' . Config::LANGUAGE . '/logging.php';
 require_once Config::ABS_PATH . '/lib/db.php';
 require_once Config::ABS_PATH . '/lib/misc.php';
 require_once Config::ABS_PATH . '/lib/popdown_handlers.php';
-try
-{
-	kotoba_session_start();
-	locale_setup();
-	$smarty = new SmartyKotobaSetup($_SESSION['language'],
-		$_SESSION['stylesheet']);
-	// Возможно завершение работы скрипта.
-	bans_check($smarty, ip2long($_SERVER['REMOTE_ADDR']));
-	if(is_guest())
-		throw new PermissionException(PermissionException::$messages['GUEST']);
-// Проверка входных параметров и отмена скрытия нити.
-	if(isset($_POST['thread']) && isset($_POST['board_name']))
-	{
-		$thread_id = threads_check_id($_POST['thread']);
-		$board_name = boards_check_name($_POST['board_name']);
-	}
-	elseif(isset($_GET['thread']) && isset($_GET['board_name']))
-	{
-		$thread_id = threads_check_id($_GET['thread']);
-		$board_name = boards_check_name($_GET['board_name']);
-	}
-	else
-	{
-		header('Location: http://z0r.de/?id=114');
-		DataExchange::releaseResources();
-		exit;
-	}
-	hidden_threads_delete($thread_id, $_SESSION['user']);
-// Перенаправление.
-	DataExchange::releaseResources();
-	header('Location: ' . Config::DIR_PATH . "/$board_name/");
-	exit;
-}
-catch(Exception $e)
-{
-	$smarty->assign('msg', $e->__toString());
-	DataExchange::releaseResources();
-	die($smarty->fetch('error.tpl'));
+
+try {
+    kotoba_session_start();
+    locale_setup();
+    $smarty = new SmartyKotobaSetup($_SESSION['language'], $_SESSION['stylesheet']);
+
+    if (($ip = ip2long($_SERVER['REMOTE_ADDR'])) === false) {
+        throw new CommonException(CommonException::$messages['REMOTE_ADDR']);
+    }
+    if (($ban = bans_check($ip)) !== false) {
+        $smarty->assign('ip', $_SERVER['REMOTE_ADDR']);
+        $smarty->assign('reason', $ban['reason']);
+        session_destroy();
+        DataExchange::releaseResources();
+        die($smarty->fetch('banned.tpl'));
+    }
+
+    if (is_guest()) {
+        throw new PermissionException(PermissionException::$messages['GUEST']);
+    }
+
+    // Проверка входных параметров и отмена скрытия нити.
+    if (isset($_POST['thread']) && isset($_POST['board_name'])) {
+        $thread_id = threads_check_id($_POST['thread']);
+        $board_name = boards_check_name($_POST['board_name']);
+    }
+    elseif (isset($_GET['thread']) && isset($_GET['board_name'])) {
+        $thread_id = threads_check_id($_GET['thread']);
+        $board_name = boards_check_name($_GET['board_name']);
+    } else {
+        header('Location: http://z0r.de/?id=114');
+        DataExchange::releaseResources();
+        exit;
+    }
+    hidden_threads_delete($thread_id, $_SESSION['user']);
+
+    // Перенаправление.
+    DataExchange::releaseResources();
+    header('Location: ' . Config::DIR_PATH . "/$board_name/");
+    exit;
+} catch(Exception $e) {
+    $smarty->assign('msg', $e->__toString());
+    DataExchange::releaseResources();
+    die($smarty->fetch('error.tpl'));
 }
 ?>
