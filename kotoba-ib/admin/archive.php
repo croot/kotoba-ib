9f9405c20c6fa22352246e0a2686585f15447924
sorcchan <sorcchan@c9e0983a-f3e1-11dd-8c4e-4bbb75a408b7>
2010-08-20 13:17:09 +0000

    Archive script fixed (any way it not actially delete data).

--- kotoba-ib/admin/archive.php
+++ kotoba-ib/admin/archive.php
@@ -1,14 +1,19 @@
 <?php
-/*************************************
+/* ***********************************
  * Этот файл является частью Kotoba. *
  * Файл license.txt содержит условия *
  * распространения Kotoba.           *
  *************************************/
-/*********************************
+/* *******************************
  * This file is part of Kotoba.  *
  * See license.txt for more info.*
  *********************************/
-// Скрипт архивирования нитей.
+
+/**
+ * Скрипт архивирования нитей.
+ * @package admscripts
+ */
+
 require '../config.php';
 require Config::ABS_PATH . '/lib/errors.php';
 require Config::ABS_PATH . '/locale/' . Config::LANGUAGE . '/errors.php';
@@ -16,135 +21,192 @@ require Config::ABS_PATH . '/lib/logging.php';
 require Config::ABS_PATH . '/locale/' . Config::LANGUAGE . '/logging.php';
 require Config::ABS_PATH . '/lib/db.php';
 require Config::ABS_PATH . '/lib/misc.php';
-try
-{
-	kotoba_session_start();
-	locale_setup();
-	$smarty = new SmartyKotobaSetup($_SESSION['language'], $_SESSION['stylesheet']);
-	bans_check($smarty, ip2long($_SERVER['REMOTE_ADDR']));	// Возможно завершение работы скрипта.
-	if(!in_array(Config::ADM_GROUP_NAME, $_SESSION['groups']))
-		throw new PermissionException(PermissionException::$messages['NOT_ADMIN']);
-	Logging::write_message(sprintf(Logging::$messages['ADMIN_FUNCTIONS_ARCHIVE'],
-				$_SESSION['user'], $_SERVER['REMOTE_ADDR']),
-			Config::ABS_PATH . '/log/' . basename(__FILE__) . '.log');
-// Получение данных.
-	$threads = threads_get_archived();
-	foreach($threads as $thread)
-	{
-		$board = boards_get_specifed($thread['board']);
-		$posts = posts_get_thread($thread['id']);
-		$posts_uploads = posts_uploads_get_posts($posts);
-		$uploads = uploads_get_posts($posts);
-// Формирование вывода.
-		$smarty->assign('board', $board);
-		$smarty->assign('thread', array($thread));
-		$view_html = $smarty->fetch('header.tpl');
-		$view_thread_html = '';
-		$view_posts_html = '';
-		$original_post = null;			// Оригинальное сообщение с допольнительными полями.
-		$original_uploads = array();	// Массив файлов, прикрепленных к оригинальному сообщению.
-		$simple_uploads = array();		// Массив файлов, прикрепленных к сообщению.
-		foreach($posts as $p)
-		{
-			// Оригинальное сообщение.
-			if($thread['original_post'] == $p['number'])
-			{
-				$p['with_files'] = false;
-				// В данной версии 1 сообщение = 1 файл.
-				foreach($posts_uploads as $pu)
-					if($p['id'] == $pu['post'])
-					{
-						// В данной версии 1 сообщение = 1 файл.
-						foreach($uploads as $u)
-							if($pu['upload'] == $u['id'])
-							{
-								$p['with_files'] = true;
-								switch($u['link_type'])
-								{
-									case Config::LINK_TYPE_VIRTUAL:
-										$u['file_link'] = Config::DIR_PATH . "/{$board['name']}/img/{$u['file']}";
-										$u['file_name'] = $u['file'];
-										if($u['is_image'])
-											$u['file_thumbnail_link'] = Config::DIR_PATH . "/{$board['name']}/thumb/{$u['thumbnail']}";
-										else
-											$u['file_thumbnail_link'] = Config::DIR_PATH . "/res/{$u['thumbnail']}";
-										break;
-									case Config::LINK_TYPE_URL:
-										$u['file_link'] = $u['file'];
-										$u['file_name'] = $u['file'];
-										$u['file_thumbnail_link'] = $u['thumbnail'];
-										break;
-									case Config::LINK_TYPE_CODE:
-									default:
-										throw new CommonException('Not supported.');
-										break;
-								}
-								array_push($original_uploads, $u);
-							}
-					}
-				$p['ip'] = long2ip($p['ip']);
-				$smarty->assign('original_post', $p);
-				$smarty->assign('original_uploads', $original_uploads);
-				$view_thread_html = $smarty->fetch('post_original.tpl');
-			}
-			else
-			{
-				$p['with_files'] = false;
-				foreach($posts_uploads as $pu)
-					if($p['id'] == $pu['post'])
-					{
-						foreach($uploads as $u)
-							if($pu['upload'] == $u['id'])
-							{
-								$p['with_files'] = true;
-								switch($u['link_type'])
-								{
-									case Config::LINK_TYPE_VIRTUAL:
-										$u['file_link'] = Config::DIR_PATH . "/{$board['name']}/img/{$u['file']}";
-										$u['file_name'] = $u['file'];
-										if($u['is_image'])
-											$u['file_thumbnail_link'] = Config::DIR_PATH . "/{$board['name']}/thumb/{$u['thumbnail']}";
-										else
-											$u['file_thumbnail_link'] = Config::DIR_PATH . "/img/{$u['thumbnail']}";
-										break;
-									case Config::LINK_TYPE_URL:
-										$u['file_link'] = $u['file'];
-										$u['file_name'] = $u['file'];
-										$u['file_thumbnail_link'] = $u['thumbnail'];
-										break;
-									case Config::LINK_TYPE_CODE:
-									default:
-										throw new CommonException('Not supported.');
-										break;
-								}
-								array_push($simple_uploads, $u);
-							}
-					}
-				$p['ip'] = long2ip($p['ip']);
-				$smarty->assign('simple_post', $p);
-				$smarty->assign('simple_uploads', $simple_uploads);
-				$view_posts_html .= $smarty->fetch('post_simple.tpl');
-				$simple_uploads = array();
-			}
-		}
-		$view_html .= $view_thread_html . $view_posts_html;
-		$view_html .= $smarty->fetch('threads_footer.tpl');
-		// Создание архивной копии.
-		$file = fopen(Config::ABS_PATH . "/{$board['name']}/arch/"
-			. "{$thread['original_post']}.html", 'w');
-		if($file)
-			fwrite($file, $view_html);
-		fclose($file);
-		// Удаление данных из базы.
-		echo "Thread {$thread['original_post']} saved.<br>";
-	}
-	DataExchange::releaseResources();
-	exit;
-}
-catch(Exception $e)
-{
-	$smarty->assign('msg', $e->__toString());
-	DataExchange::releaseResources();
-	die($smarty->fetch('error.tpl'));
+
+try {
+    // Initialization.
+    kotoba_session_start();
+    locale_setup();
+    $smarty = new SmartyKotobaSetup($_SESSION['language'], $_SESSION['stylesheet']);
+
+    // Check if remote host was banned.
+    if (($ip = ip2long($_SERVER['REMOTE_ADDR'])) === false) {
+        throw new CommonException(CommonException::$messages['REMOTE_ADDR']);
+    }
+    if (($ban = bans_check($ip)) !== false) {
+        $smarty->assign('ip', $_SERVER['REMOTE_ADDR']);
+        $smarty->assign('reason', $ban['reason']);
+        session_destroy();
+        DataExchange::releaseResources();
+        die($smarty->fetch('banned.tpl'));
+    }
+
+    // Check permission and write message to log file.
+    if (!is_admin()) {
+        throw new PermissionException(PermissionException::$messages['NOT_ADMIN']);
+    }
+    Logging::write_msg(Config::ABS_PATH . '/log/' . basename(__FILE__) . '.log',
+            Logging::$messages['ADMIN_FUNCTIONS_ARCHIVE'],
+            $_SESSION['user'], $_SERVER['REMOTE_ADDR']);
+    Logging::close_log();
+
+    $threads = threads_get_archived();
+    $smarty->assign('ATTACHMENT_TYPE_FILE', Config::ATTACHMENT_TYPE_FILE);
+    $smarty->assign('ATTACHMENT_TYPE_LINK', Config::ATTACHMENT_TYPE_LINK);
+    $smarty->assign('ATTACHMENT_TYPE_VIDEO', Config::ATTACHMENT_TYPE_VIDEO);
+    $smarty->assign('ATTACHMENT_TYPE_IMAGE', Config::ATTACHMENT_TYPE_IMAGE);
+    foreach ($threads as $thread) {
+
+        // Receive data for curent thread.
+        $board = boards_get_by_id($thread['board']);
+        $posts = posts_get_by_thread($thread['id']);
+        $posts_attachments = posts_attachments_get_by_posts($posts);
+        $attachments = attachments_get_by_posts($posts);
+
+        // Generate output.
+        $smarty->assign('board', $board);
+        $smarty->assign('thread', array($thread));
+        $view_html = $smarty->fetch('header.tpl');
+        $view_thread_html = '';
+        $view_posts_html = '';
+        $original_post = null;              // Оригинальное сообщение с допольнительными полями.
+        $original_attachments = array();    // Массив файлов, прикрепленных к оригинальному сообщению.
+        $simple_attachments = array();      // Массив файлов, прикрепленных к сообщению.
+
+        foreach ($posts as $p) {
+
+            // Default name.
+            if (!$board['force_anonymous'] && $board['default_name'] !== null && $p['name'] === null) {
+                $p['name'] = $board['default_name'];
+            }
+
+            // Original post.
+            if ($thread['original_post'] == $p['number']) {
+
+                // By default post have no attachments. This is fake field.
+                $p['with_attachments'] = false;
+
+                foreach ($posts_attachments as $pa) {
+                    if ($p['id'] == $pa['post']) {
+                        foreach ($attachments as $a) {
+                            if ($a['attachment_type'] == $pa['attachment_type']) {
+                                switch ($a['attachment_type']) {
+                                    case Config::ATTACHMENT_TYPE_FILE:
+                                        if ($a['id'] == $pa['file']) {
+                                            $a['file_link'] = Config::DIR_PATH . "/{$board['name']}/other/{$a['name']}";
+                                            $a['thumbnail_link'] = Config::DIR_PATH . "/img/{$a['thumbnail']}";
+                                            $p['with_attachments'] = true;
+                                            array_push($original_attachments, $a);
+                                        }
+                                        break;
+                                    case Config::ATTACHMENT_TYPE_IMAGE:
+                                        if ($a['id'] == $pa['image']) {
+                                            $a['image_link'] = Config::DIR_PATH . "/{$board['name']}/img/{$a['name']}";
+                                            $a['thumbnail_link'] = Config::DIR_PATH . "/{$board['name']}/thumb/{$a['thumbnail']}";
+                                            $p['with_attachments'] = true;
+                                            array_push($original_attachments, $a);
+                                        }
+                                        break;
+                                    case Config::ATTACHMENT_TYPE_LINK:
+                                        if ($a['id'] == $pa['link']) {
+                                            $p['with_attachments'] = true;
+                                            array_push($original_attachments, $a);
+                                        }
+                                        break;
+                                    case Config::ATTACHMENT_TYPE_VIDEO:
+                                        if ($a['id'] == $pa['video']) {
+                                            $smarty->assign('code', $a['code']);
+                                            $a['video_link'] = $smarty->fetch('youtube.tpl');
+                                            $p['with_attachments'] = true;
+                                            array_push($original_attachments, $a);
+                                        }
+                                        break;
+                                    default:
+                                        throw new CommonException('Not supported.');
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+                $p['ip'] = long2ip($p['ip']);
+                $smarty->assign('original_post', $p);
+                $smarty->assign('original_attachments', $original_attachments);
+                $view_thread_html = $smarty->fetch('post_original.tpl');
+            } else {
+                
+                // By default post have no attachments. This is fake field.
+                $p['with_attachments'] = false;
+
+                foreach ($posts_attachments as $pa) {
+                    if ($p['id'] == $pa['post']) {
+                        foreach ($attachments as $a) {
+                            if ($a['attachment_type'] == $pa['attachment_type']) {
+                                switch ($a['attachment_type']) {
+                                    case Config::ATTACHMENT_TYPE_FILE:
+                                        if ($a['id'] == $pa['file']) {
+                                            $a['file_link'] = Config::DIR_PATH . "/{$board['name']}/other/{$a['name']}";
+                                            $a['thumbnail_link'] = Config::DIR_PATH . "/img/{$a['thumbnail']}";
+                                            $p['with_attachments'] = true;
+                                            array_push($simple_attachments, $a);
+                                        }
+                                        break;
+                                    case Config::ATTACHMENT_TYPE_IMAGE:
+                                        if ($a['id'] == $pa['image']) {
+                                            $a['image_link'] = Config::DIR_PATH . "/{$board['name']}/img/{$a['name']}";
+                                            $a['thumbnail_link'] = Config::DIR_PATH . "/{$board['name']}/thumb/{$a['thumbnail']}";
+                                            $p['with_attachments'] = true;
+                                            array_push($simple_attachments, $a);
+                                        }
+                                        break;
+                                    case Config::ATTACHMENT_TYPE_LINK:
+                                        if ($a['id'] == $pa['link']) {
+                                            $p['with_attachments'] = true;
+                                            array_push($simple_attachments, $a);
+                                        }
+                                        break;
+                                    case Config::ATTACHMENT_TYPE_VIDEO:
+                                        if ($a['id'] == $pa['video']) {
+                                            $smarty->assign('code', $a['code']);
+                                            $a['video_link'] = $smarty->fetch('youtube.tpl');
+                                            $p['with_attachments'] = true;
+                                            array_push($simple_attachments, $a);
+                                        }
+                                        break;
+                                    default:
+                                        throw new CommonException('Not supported.');
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+                $p['ip'] = long2ip($p['ip']);
+                $smarty->assign('simple_post', $p);
+                $smarty->assign('simple_attachments', $simple_attachments);
+                $view_posts_html .= $smarty->fetch('post_simple.tpl');
+                $simple_attachments = array();
+            }
+        }
+        $view_html .= $view_thread_html . $view_posts_html;
+        $view_html .= $smarty->fetch('threads_footer.tpl');
+
+        // Write output to file.
+        $file = fopen(Config::ABS_PATH . "/{$board['name']}/arch/" . "{$thread['original_post']}.html", 'w');
+        if ($file) {
+            fwrite($file, $view_html);
+        }
+        fclose($file);
+
+        // Remove data from database.
+
+        echo "Thread {$thread['original_post']} saved.<br>";
+    }
+
+    DataExchange::releaseResources();
+    exit;
+} catch(Exception $e) {
+    $smarty->assign('msg', $e->__toString());
+    DataExchange::releaseResources();
+    die($smarty->fetch('error.tpl'));
 }
 ?>
