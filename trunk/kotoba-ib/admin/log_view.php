08e5e8ff133718acc37696953ca9b9402074ef89
sorcchan <sorcchan@c9e0983a-f3e1-11dd-8c4e-4bbb75a408b7>
2011-06-01 15:21:33 +0000

    Добавлена возможность выбрать количество строк при просмотре лога. Исправлен баг с отображением досок в navbar-е в скрипте редактирования досок.

--- trunk/kotoba-ib/admin/log_view.php
+++ trunk/kotoba-ib/admin/log_view.php
@@ -1,19 +1,11 @@
 <?php
-/* ***********************************
- * Этот файл является частью Kotoba. *
- * Файл license.txt содержит условия *
- * распространения Kotoba.           *
- *************************************/
 /* *******************************
  * This file is part of Kotoba.  *
  * See license.txt for more info.*
  *********************************/
 
-/**
- * Log view script.
- */
+// Log view script.
 
-/***/
 require_once '../config.php';
 require_once Config::ABS_PATH . '/lib/errors.php';
 require_once Config::ABS_PATH . '/lib/logging.php';
@@ -48,34 +40,53 @@ try {
     }
     call_user_func(Logging::$f['LOG_VIEW_USE']);
 
-    date_default_timezone_set(Config::DEFAULT_TIMEZONE);
-    $logf = fopen(Config::ABS_PATH . '/log/actions-' . date('Y-m-d') . '.log', 'r');
-    $i = 0;
-    while (($line = fgets($logf))) {
-        $log[$i] = preg_split('/\|/', $line, -1, PREG_SPLIT_NO_EMPTY);
-        $log[$i][4] = htmlentities($log[$i][4], ENT_QUOTES, Config::MB_ENCODING);
-        $i++;
+    $categories = categories_get_all();
+    $boards = boards_get_all();
+
+    // Make category-boards tree for navigation panel.
+    foreach ($categories as &$c) {
+        $c['boards'] = array();
+        foreach ($boards as $b) {
+            if ($b['category'] == $c['id'] && !in_array($b['name'], Config::$INVISIBLE_BOARDS)) {
+                array_push($c['boards'], $b);
+            }
+        }
     }
 
-    // TODO Make optional.
-    $records_count = 10;
+    if (isset($_REQUEST['log_view'])) {
+        $kargv = $_REQUEST['log_view'];
+        $records_count = kotoba_intval($kargv['records_count']);
+        $records_count = $records_count > 0 ? $records_count : 10;
+        $smarty->assign('records_count_prev', $records_count);
 
-    if ( ($n = count($log)) > $records_count) {
-        $log = array_slice($log, $n - $records_count, $records_count);
+        date_default_timezone_set(Config::DEFAULT_TIMEZONE);
+        $logf = fopen(Config::ABS_PATH . '/log/actions-' . date('Y-m-d') . '.log', 'r');
+        $i = 0;
+        while (($line = fgets($logf))) {
+            $log[$i] = preg_split('/\|/', $line, -1, PREG_SPLIT_NO_EMPTY);
+            $log[$i][4] = htmlentities($log[$i][4], ENT_QUOTES, Config::MB_ENCODING);
+            $i++;
+        }
+
+        if ( ($n = count($log)) > $records_count) {
+            $log = array_slice($log, $n - $records_count, $records_count);
+        }
+        $log = array_reverse($log);
+
+        $smarty->assign('log', $log);
+    } else {
+        $smarty->assign('records_count_prev', 10);
     }
-    $log = array_reverse($log);
 
     // Generate html code of log view page and display it.
     $smarty->assign('show_control', is_admin() || is_mod());
-    $smarty->assign('boards', boards_get_all());
-    $smarty->assign('log', $log);
+    $smarty->assign('categories', $categories);
+    $smarty->assign('boards', $boards);
     $smarty->display('log_view.tpl');
 
     // Cleanup.
     DataExchange::releaseResources();
     Logging::close_log();
-
-    exit(0);
 } catch (Exception $e) {
     $smarty->assign('msg', $e->__toString());
     DataExchange::releaseResources();
--- trunk/kotoba-ib/smarty/kotoba/templates/locale/eng/log_view.tpl
+++ trunk/kotoba-ib/smarty/kotoba/templates/locale/eng/log_view.tpl
@@ -10,22 +10,40 @@ Variables:
     $DIR_PATH - path from server document root to index.php directory (see config.default).
     $STYLESHEET - stylesheet (see config.default).
     $show_control - show link to manage page.
+    $categories - categories.
     $boards - boards.
+    $records_count_prev - previously selected count of rows.
     $log - log.
 *}
 {include file='header.tpl' DIR_PATH=$DIR_PATH STYLESHEET=$STYLESHEET page_title="Log view"}
 
 {include file='adminbar.tpl' DIR_PATH=$DIR_PATH show_control=$show_control}
 
-{include file='navbar.tpl' DIR_PATH=$DIR_PATH boards=$boards}
+{include file='navbar.tpl' DIR_PATH=$DIR_PATH categories=$categories boards=$boards}
 
 <div class="logo">Log view</div>
 
 <hr>
+<div class="postarea">
+<form name="postform"
+      id="postform"
+      action="{$DIR_PATH}/admin/log_view.php"
+      method="post"
+      enctype="multipart/form-data">
+Rows count: <input type="text" name="log_view[records_count]" value="{$records_count_prev}"> <input type="submit" value="Select">
+</form>
+</div>
+<br>
 <table cellspacing="2" cellpadding="1" border="1" width="100%">
 <tr><th>Date</th><th>User id</th><th>User groups</th><th>IP-address</th><th>Message</th></tr>
-{section name=i loop=$log}
-    <tr><td>{$log[i][0]}</td><td>{$log[i][1]}</td><td>{$log[i][2]}</td><td>{$log[i][3]}</td><td>{$log[i][4]}</td></tr>
-{/section}
+{if isset($log)}{strip}{section name=i loop=$log}
+    <tr>
+        <td>{$log[i][0]}</td>
+        <td>{$log[i][1]}</td>
+        <td>{$log[i][2]}</td>
+        <td>{$log[i][3]}</td>
+        <td>{$log[i][4]}</td>
+    </tr>
+{/section}{/strip}{/if}
 </table>
 {include file='footer.tpl'}
